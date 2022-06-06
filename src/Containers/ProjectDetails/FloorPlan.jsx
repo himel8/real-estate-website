0a8dd566d70d7a653c81ae1283cb1f604fe90a83
himel8 <himelkhan290@gmail.com>
2022-06-06 14:21:43 +0600

    desktop show added

--- src/Containers/ProjectDetails/FloorPlan.jsx
+++ src/Containers/ProjectDetails/FloorPlan.jsx
@@ -3,14 +3,13 @@ import DesktopFloorPlan from "../../Components/DesktopFloorPlan";
 import Title from "../../Components/Title";
 
 const FloorPlan = ({ plans }) => {
+  const desktopView = window.innerWidth > 768;
   return (
     <div className="w-[95%] mx-auto pt-12  pb-12  md:w-[85%]">
       <div className="text-center">
         <Title title={"floor plan"} />
       </div>
-      <div>
-        <DesktopFloorPlan plans={plans} />
-      </div>
+      <div>{desktopView && <DesktopFloorPlan plans={plans} />}</div>
     </div>
   );
 };
